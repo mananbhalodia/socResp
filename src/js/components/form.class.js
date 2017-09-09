daa142a6149f7e724b93cd248b1fa7645bb76698
Noam Dorogoyer <noam@Noams-MacBook-Air.local>
2017-09-09 17:38:59 -0400

    added forms, improved menu, evnt handlers

---
+++ src/js/components/form.class.js
@@ -0,0 +1,46 @@
+import { Form } from 'mobx-react-form';
+import validatorjs from 'validatorjs';
+
+export default class MyForm extends Form {
+
+  plugins() {
+    return { dvr: validatorjs };
+  }
+  
+  setup() {
+    return {
+      fields: [{
+        name: 'email',
+        label: 'Email',
+        placeholder: 'Insert Email',
+        rules: 'required|email|string|between:5,25',
+        value: 's.jobs@apple.com'
+      }, {
+        name: 'password',
+        label: 'Password',
+        placeholder: 'Insert Password',
+        rules: 'required|string|between:5,25',
+      }, {
+        name: 'passwordConfirm',
+        label: 'Password Confirmation',
+        placeholder: 'Confirm Password',
+        rules: 'required|string|same:password',
+      }],
+    };
+  }
+
+  hooks() {
+    return {
+      onSuccess(form) {
+        alert('Form is valid! Send the request here.');
+        // get field values
+        console.log('Form Values!', form.values());
+      },
+      onError(form) {
+        alert('Form has errors!');
+        // get all form errors
+        console.log('All form errors', form.errors());
+      }
+    };
+  }
+}
